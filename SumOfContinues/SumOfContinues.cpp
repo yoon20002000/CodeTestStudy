b46ab0bca2d8813546d88da1ac90a6bb40d8d6b4
yoon20002000 <yoon20002000@naver.com>
2024-10-19 22:33:54 +0900

    Add SumOfContinues

---
+++ SumOfContinues/SumOfContinues.cpp
@@ -0,0 +1,19 @@
+﻿#include "SumOfContinues.h"
+
+SumOfContinues::SumOfContinues(int num, int total)
+{
+}
+
+vector<int> SumOfContinues::solution(const int num, const  int total)
+{
+    vector<int> answer;
+    answer.reserve(num);
+    const int x = (static_cast<float>(total) / num) - (static_cast<float>(num) / 2) + 0.5f;
+    
+    for(int i = 0 ; i < num ; ++i)
+    {
+        answer.push_back(x + i);
+    }
+    
+    return answer;
+}
---
+++ SumOfContinues/SumOfContinues.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <vector>
+using namespace std;
+class SumOfContinues
+{
+public:
+    SumOfContinues(int num, int total);
+private:
+    vector<int> solution(const int num,const int total);
+    
+};
