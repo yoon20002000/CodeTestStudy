b8d3bce8e71c27476bacc5dd63e5f9088142e916
yoon20002000 <yoon20002000@naver.com>
2024-10-21 12:38:26 +0900

    ShiftString Add

---
+++ ShiftString/ShiftString.cpp
@@ -0,0 +1,46 @@
+﻿#include "ShiftString.h"
+
+ShiftString::ShiftString(string& input, string& target)
+{
+    solution(input, target);
+}
+
+int ShiftString::solution(string& input, string& target)
+{
+    int answer = 0;
+
+    char first = input[0];
+    int length = input.length();
+    vector<int> findIndexs;
+    findIndexs.reserve(length);
+    
+    for(int i = 0 ; i < length; ++i)
+    {
+        if(first == target[i])
+        {
+            findIndexs.push_back(i);
+        }
+    }
+
+    for(int findIndex : findIndexs)
+    {
+        // % length 하면 됨
+        bool isMatch = true;
+        for(int i = 0 ; i < length; ++i)
+        {
+            int targetPivot = (findIndex + i) % length;
+            if(target[targetPivot] != input[i])
+            {
+                continue;
+            }
+        }
+        if(isMatch)
+        {
+            return findIndex;
+        }
+    }
+
+    
+    
+    return answer;
+}
---
+++ ShiftString/ShiftString.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+#include <vector>
+#include <string>
+using namespace std;
+class ShiftString
+{
+public:
+    ShiftString(string& input, string& target);
+private:
+    int solution(string&  input, string& target);
+};
