9187a86332f3e617de424a193268c4bd620a4ff6
yoon20002000 <yoon20002000@naver.com>
2024-10-24 18:13:56 +0900

    DataAnalze

---
+++ PCCE/DataAnalyze/DataAnalyze.cpp
@@ -0,0 +1,31 @@
+﻿#include "DataAnalyze.h"
+#include <algorithm>
+DataAnalyze::DataAnalyze(vector<vector<int>> data, string ext, int val_ext, string sort_by)
+{
+    solution(data,ext,val_ext,sort_by);
+}
+
+vector<vector<int>> DataAnalyze::solution(vector<vector<int>> data, string ext, int val_ext, string sort_by)
+{
+    vector<vector<int>> answer;
+
+    int dataIndex = getDataIndex(ext);
+    int sortIndex = getDataIndex(sort_by);
+
+    for (vector<int> data_raw : data)
+    {
+        if(data_raw[dataIndex]< val_ext)
+        {
+            answer.push_back(data_raw);
+        }
+    }
+
+    sort(answer.begin(),answer.end(),[sortIndex](const vector<int>& a, const vector<int>& b)
+    {
+
+        return a[sortIndex] < b[sortIndex];
+        
+    });
+
+    return answer;
+}
---
+++ PCCE/DataAnalyze/DataAnalyze.h
@@ -0,0 +1,38 @@
+﻿#pragma once
+#include <vector>
+#include <string>
+using namespace std;
+
+class DataAnalyze
+{
+public:
+    DataAnalyze(vector<vector<int>> data, string ext, int val_ext, string sort_by);
+private:
+    vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) ;
+
+    uint8_t getDataIndex(const string& data_type)
+    {
+        if(data_type == "code")
+        {
+            return 0;
+        }
+        else if(data_type == "date")
+        {
+            return 1;
+        }
+        else if(data_type == "maximum")
+        {
+            return 2;   
+        }
+        else
+        {
+            return 3;
+        }
+    }
+    bool isOverDate(int dateA, int dateB)
+    {
+        return dateA > dateB;
+    }
+
+   
+};
