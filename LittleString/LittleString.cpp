f00dd4df3ce2b9a04affcbb611a1af54ec804b38
yoon20002000 <yoon20002000@naver.com>
2024-10-26 00:32:56 +0900

    Little String

---
+++ LittleString/LittleString.cpp
@@ -0,0 +1,26 @@
+﻿#include "LittleString.h"
+
+LittleString::LittleString(const string& t, const string& p)
+{
+    solution(t,p);
+}
+
+int LittleString::solution(const string& t, const string& p)
+{
+    int answer = 0;
+    size_t pSize = p.length();
+    long pCompare = std::stol(p);
+
+    for (int i = 0; i + pSize <= t.length(); ++i)
+    {
+        string sub = t.substr(i, pSize);
+        const long cur = std::stol(sub);
+
+        if (cur <= pCompare)
+        {
+            ++answer;
+        }
+    }
+
+    return answer;
+}
---
+++ LittleString/LittleString.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <string>
+using namespace std;
+class LittleString
+{
+public:
+    LittleString(const string& t, const string& p);
+    int solution(const string& t, const string& p);
+};
