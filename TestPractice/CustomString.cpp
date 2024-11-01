88858d53ce291722485fc720ee243a1a4fb63cd1
yoon20002000 <yoon20002000@naver.com>
2024-11-01 21:19:09 +0900

    Custom String

---
+++ TestPractice/CustomString.cpp
@@ -0,0 +1,85 @@
+﻿#include "CustomString.h"
+
+#include <cstring>
+#include <string>
+
+CustomString::CustomString() : length(0), capacity(0)
+{
+}
+
+CustomString::CustomString(const char* str)
+{
+    length = strlen(str);
+    data = new char[length + 1];
+    capacity = length;
+    strcpy(data, str);
+}
+
+CustomString::CustomString(const CustomString& other)
+{
+    length = other.length;
+    data = new char[length + 1];
+    capacity = other.capacity;
+    strcpy(data, other.data);
+}
+
+CustomString CustomString::operator+(const CustomString& other) const
+{
+    size_t newlength = other.length + length;
+    char* newData = new char[newlength + 1];
+    std::strcpy(newData, data);
+    std::strcat(newData, other.data);
+
+    CustomString newString (newData);
+
+    delete[] newData;
+    
+    return newString;
+}
+
+CustomString& CustomString::operator=(const CustomString& other) 
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+
+    delete[] data;
+    length = other.length;
+    if(other.capacity > capacity)
+    {
+        capacity = other.capacity;
+        data = new char[length + 1];    
+    }
+    
+    std::strcpy(data, other.data);
+
+    return *this;
+}
+
+CustomString& CustomString::operator=(CustomString&& other)
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+
+    delete[] data;
+    data = other.data;
+    length = other.length;
+
+    other.data = nullptr;
+    other.length = 0;
+
+    return *this;
+}
+
+const char& CustomString::operator[](size_t index) const
+{
+    return data[index];
+}
+
+CustomString::~CustomString()
+{
+    delete[] data;
+}
---
+++ TestPractice/CustomString.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+class CustomString
+{
+private:
+    char* data = '';
+    size_t length;
+    size_t capacity;
+public:
+    CustomString();
+    CustomString(const char* str);
+    CustomString(const CustomString& other);
+    CustomString operator+(const CustomString& other) const;
+    CustomString& operator=(const CustomString& other);
+    CustomString& operator=(CustomString&& other) noexcept;
+    const char& operator[](size_t index) const;
+    ~CustomString();
+};
