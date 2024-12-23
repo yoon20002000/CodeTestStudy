4ba62e9e215a570a5ae37dee3383dd17d7a1e2e0
yoon20002000 <yoon20002000@naver.com>
2024-12-24 00:57:29 +0900

    Puddle DP

---
+++ Programmers/Puddle/Puddle.cpp
@@ -0,0 +1,42 @@
+﻿#include "Puddle.h"
+
+int Puddle::solution(int m, int n, vector<vector<int>> puddles)
+{
+    vector<vector<int>> dp(n,vector<int>(m,0));
+
+    for (vector<int> puddle : puddles)
+    {
+        dp[puddle[1]-1][puddle[0]-1] = -1;
+    }
+
+    dp[0][0] = 1;
+    
+    for (int i = 0 ; i < n ; ++i)
+    {
+        for (int j = 0 ; j < m ; ++j)
+        {
+            if (dp[i][j] == -1)
+            {
+                dp[i][j] = 0;
+                continue;
+            }
+            else
+            {
+                if (i >0)
+                {
+                    dp[i][j] += dp[i-1][j];
+                    dp[i][j] %= 1000000007;
+                }
+
+                if (j > 0)
+                {
+                    dp[i][j] += dp[i][j-1];
+                    dp[i][j] %= 1000000007;
+                }
+            }
+        }
+    }
+    
+    return dp[n-1][m-1] ;
+}
+
---
+++ Programmers/Puddle/Puddle.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include <vector>
+using namespace std;
+
+class Puddle
+{
+public:
+    int solution(int m, int n, vector<vector<int>> puddles);
+};
